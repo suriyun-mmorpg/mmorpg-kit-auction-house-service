af0f28a5426710807a2f9ef7d47363017c7a88d8
Insthync <ittipon.bay@gmail.com>
2021-10-31 20:29:05 +0700

    Generate mail client

---
+++ prisma/generated/mail-client/index-browser.js
@@ -0,0 +1,130 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const {
+  Decimal
+} = require('./runtime/index-browser')
+
+
+const Prisma = {}
+
+exports.Prisma = Prisma
+
+/**
+ * Prisma Client JS version: 2.22.1
+ * Query Engine version: 60cc71d884972ab4e897f0277c4b84383dddaf6c
+ */
+Prisma.prismaVersion = {
+  client: "2.22.1",
+  engine: "60cc71d884972ab4e897f0277c4b84383dddaf6c"
+}
+
+Prisma.PrismaClientKnownRequestError = () => {
+  throw new Error(`PrismaClientKnownRequestError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)};
+Prisma.PrismaClientUnknownRequestError = () => {
+  throw new Error(`PrismaClientUnknownRequestError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientRustPanicError = () => {
+  throw new Error(`PrismaClientRustPanicError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientInitializationError = () => {
+  throw new Error(`PrismaClientInitializationError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientValidationError = () => {
+  throw new Error(`PrismaClientValidationError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.Decimal = Decimal
+
+/**
+ * Re-export of sql-template-tag
+ */
+
+Prisma.sql = () => {
+  throw new Error(`sqltag is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.empty = () => {
+  throw new Error(`empty is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.join = () => {
+  throw new Error(`join is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.raw = () => {
+  throw new Error(`raw is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.validator = () => (val) => val
+
+/**
+ * Enums
+ */
+// Based on
+// https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275
+function makeEnum(x) { return x; }
+
+exports.Prisma.AuctionScalarFieldEnum = makeEnum({
+  id: 'id',
+  buyoutPrice: 'buyoutPrice',
+  bidPrice: 'bidPrice',
+  startBidPrice: 'startBidPrice',
+  sellerId: 'sellerId',
+  sellerName: 'sellerName',
+  isEnd: 'isEnd',
+  isBuyout: 'isBuyout',
+  buyerId: 'buyerId',
+  buyerName: 'buyerName',
+  itemDataId: 'itemDataId',
+  itemLevel: 'itemLevel',
+  itemAmount: 'itemAmount',
+  itemDurability: 'itemDurability',
+  itemExp: 'itemExp',
+  itemLockRemainsDuration: 'itemLockRemainsDuration',
+  itemExpireTime: 'itemExpireTime',
+  itemRandomSeed: 'itemRandomSeed',
+  itemSockets: 'itemSockets',
+  endedAt: 'endedAt',
+  createdAt: 'createdAt'
+});
+
+exports.Prisma.Auction_bid_logsScalarFieldEnum = makeEnum({
+  id: 'id',
+  auctionId: 'auctionId',
+  buyerId: 'buyerId',
+  buyerName: 'buyerName',
+  bidPrice: 'bidPrice',
+  isBuyout: 'isBuyout'
+});
+
+exports.Prisma.SortOrder = makeEnum({
+  asc: 'asc',
+  desc: 'desc'
+});
+
+
+exports.Prisma.ModelName = makeEnum({
+  auction: 'auction',
+  auction_bid_logs: 'auction_bid_logs'
+});
+
+/**
+ * Create the Client
+ */
+class PrismaClient {
+  constructor() {
+    throw new Error(
+      `PrismaClient is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+    )
+  }
+}
+exports.PrismaClient = PrismaClient
+
+Object.assign(exports, Prisma)
